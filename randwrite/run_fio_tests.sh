2e3bcce1c4d802e528e7823e7241410de0ceeeee
橘子不是唯一的水果 <90956796+lixie021@users.noreply.github.com>
2024-08-21 16:42:31 +0800

    docs:update readme.md

--- randwrite/run_fio_tests.sh
+++ randwrite/run_fio_tests.sh
@@ -4,8 +4,11 @@
 NUMJOBS=${1:-1}  # 默认为1，可以通过命令行参数覆盖
 MODE=${2:-randread}  # 默认为randread，可以通过命令行参数覆盖
 BLOCK_SIZE=${3:-4k}  # 默认为4k，可以通过命令行参数覆盖
+OUTPUT_BASE_DIR=${4:-env_json_results}  # 默认为env_json_results，可以通过命令行参数覆盖
 FILESIZE=2G  # 文件大小
-OUTPUT_DIR="json_results"  # JSON 文件存放目录
+
+# 根据模式和块大小定义输出目录
+OUTPUT_DIR="./${OUTPUT_BASE_DIR}/${MODE}-${BLOCK_SIZE}"
 
 # 创建输出目录
 mkdir -p $OUTPUT_DIR
@@ -16,9 +19,9 @@ iodepths=("1" "2" "4" "8" "16")
 for IODEPTH in "${iodepths[@]}"; do
   # 创建配置文件名称
   config_file="${NUMJOBS}-proc-${MODE}-${BLOCK_SIZE}-${IODEPTH}.fio"
-  
+
   # 创建配置文件
-  cat << EOF > $config_file
+  cat << EOF2 > $config_file
 [disktest]
 ioengine=libaio
 iodepth=${IODEPTH}
@@ -31,13 +34,13 @@ size=${FILESIZE}
 runtime=30
 time_based
 group_reporting
-EOF
+EOF2
 
   # 定义输出文件名称
   output_file="${OUTPUT_DIR}/${NUMJOBS}-proc-${MODE}-${BLOCK_SIZE}-${IODEPTH}.json"
 
   # 运行fio测试
-  fio --output-format=json --output="${output_file}" $config_file
+  fio --output-format=json --output="$output_file" $config_file
 
   # 输出完成信息
   echo "Prepared to run fio with numjobs=${NUMJOBS}, rw=${MODE}, bs=${BLOCK_SIZE}, and iodepth=${IODEPTH}"
